9e677b20c1c0cc9755c03f32b4f777fbc5300830
Pablo Salce <salcepablo13@gmail.com>
2020-10-31 15:12:00 -0700

    added randomizing the answers

--- src/components/TriviaComponent.js
+++ src/components/TriviaComponent.js
@@ -5,20 +5,38 @@ class Trivia extends Component {
     constructor(props) {
         super(props);
     }
+
+    shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    return array;
+    }
+
     //setting the state for selected question from the rendered question title cards
     randomTriviaContentGenerator() {
       const randomIndexPosition = Math.floor(Math.random() * this.props.triviaContent.length - 1);
       const trivia = this.props.triviaContent[randomIndexPosition];
+      const answers = trivia.incorrect.concat(trivia.correct);
+      console.log(answers);
+      // const randomAnswer = Math.floor(Math.random() * answers.length - 1;
+      const randomAnswer = this.shuffle(answers);
+      console.log(randomAnswer);
+
+
       return (
-        <div className="col-md-5 m-1">
+        <div className="col-md-12 m-1">
           {/* renders the actual small card with only the question */}
             <Card>
               <CardTitle>{trivia.question}</CardTitle>  
               <div>
-                <Button>{trivia.incorrect[1]}</Button>
-                <Button>{trivia.incorrect[2]}</Button>
-                <Button>{trivia.incorrect[0]}</Button>
-                <Button>{trivia.correct}</Button>
+                <Button className="col-md-4 m-1">{randomAnswer[0]}</Button>
+                <Button className="col-md-4 m-1">{randomAnswer[1]}</Button>
+              </div>
+              <div>
+                <Button className="col-md-4 m-1">{randomAnswer[2]}</Button>
+                <Button className="col-md-4 m-1">{randomAnswer[3]}</Button>
               </div>
             </Card>
         </div>
